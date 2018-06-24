477208235355e50c9e314a62d87f52baa63613e9
John DeJesus <j.dejesus22@gmail.com>
2018-06-23 23:04:36 -0400

    Get titles of clusters

--- RegentsDataCleaning.py
+++ RegentsDataCleaning.py
@@ -1,31 +1,58 @@
-#Regents Data Cleaning
-
-import numpy as np
+# Regents Data Cleaning
 import pandas as pd
-import matplotlib.pyplot as plt
+import os
 
-#geo regents as of Aug 2017
+# geo regents as of Aug 2017
 geo=pd.read_excel('GeoQuestionBreakdown.xlsx')
-geo['Type']=geo['Type'].map({'Multiple':'MC', 'Constructed': 'CR'})
+geo['Type'] = geo['Type'].map({'Multiple': 'MC', 'Constructed': 'CR'})
 
 geo.to_excel('PreppedGeoQuestionBreakdown.xlsx', index=False)
 
 #############################################################################################
-#Alg 1 regents as of Aug 2017
-alg=pd.read_excel('Alg1QuestionBreakdownxl.xlsx')
+# Alg 1 regents as of Aug 2017
+alg = pd.read_excel('Alg1QuestionBreakdownxl.xlsx')
 alg['Type']=alg['Type'].map({'Multiple':'MC', 'Constructed': 'CR'})
 
 alg.to_excel('PreppedAlg1QuestionBreakdown.xlsx', index=False)
 
+os.chdir('D:\\MathRegentsDataFiles')
+alg = pd.read_csv('PreppedAlg1QuestionBreakdown.csv', encoding='latin1')
+alg_clusters = sorted(alg.Cluster.unique().tolist())
+alg_clustermeanings = ['Perform arithmetic operations on polynomials.',
+                       'Understand the relationship between zeros and factors of polynomials.',
+                       'Create equations that describe numbers or relationships.',
+                       'Understand solving equations as a process of reasoning and explain the reasoning.',
+                       'Solve equations and inequalities in one variable.',
+                       'Solve systems of equations.',
+                       'Represent and solve equations and inequalities graphically.',
+                       'Interpret the structure of expressions.',
+                       'Write expressions in equivalent forms to solve problems.',
+                       'Build a function that models a relationship between two quantities.',
+                       'Build new functions from existing functions.',
+                       'Understand the concept of a function and use function notation.',
+                       'Interpret functions that arise in applications in terms of the context.',
+                       'Analyze functions using different representations.',
+                       'Construct and compare linear, quadratic, and exponential models and solve problems.',
+                       'Interpret expressions for functions in terms of the situation they model.',
+                       'Reason quantitatively and use units to solve problems.',
+                       'Use properties of rational and irrational numbers.',
+                       'Summarize, represent, and interpret data on a single count or measurement variable',
+                       'Summarize, represent, and interpret data on two categorical and quantitative variables',
+                       'Interpret linear models']
+
+# create dictionary and map to new cluster title column
+alg_clusterDict = dict(zip(alg_clusters, alg_clustermeanings))
+alg['ClusterTitle'] = alg['Cluster'].map(alg_clusterDict)
+alg.to_csv('PreppedAlg1QuestionBreakdown.csv', index=False)
 #############################################################################################
-#Alg 2 regents as of Aug 2017
-alg=pd.read_excel('Alg2QuestionBreakdownxl.xlsx')
+# Alg 2 regents as of Aug 2017
+alg = pd.read_excel('Alg2QuestionBreakdownxl.xlsx')
 alg['Type']=alg['Type'].map({'Multiple':'MC', 'Constructed': 'CR'})
 
 alg.to_excel('PreppedAlg2QuestionBreakdown.xlsx', index=False)
 
 ###########################################################################################
-#mapping meanings to cluster codes for geometry
+# mapping meanings to cluster codes for geometry
 geo=pd.read_excel('PreppedGeoQuestionBreakdown.xlsx')
 
 clusters=sorted(geo['Cluster'].unique().tolist())
@@ -44,11 +71,11 @@ clusterMeanings=['Understand and apply theorems about circles',
                  'Prove theorems involving similarity',
                  'Define trigonometric ratios and solve problems involving right triangles']
 
-#create dictionary and map to new cluster title column
+# create dictionary and map to new cluster title column
 clusterDict=dict(zip(clusters,clusterMeanings))
 geo['ClusterTitle']=geo['Cluster'].map(clusterDict)
 
-#update excel file
+# update excel file
 geo.to_excel('PreppedGeoQuestionBreakdown.xlsx', index=False)
                  
 
